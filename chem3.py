60a232d29a35b40dbc81c3568d77813b57e01dc7
shmeez <88584106+shmeez@users.noreply.github.com>
2021-08-07 10:30:57 -0400

    Update chem3.py

--- chem3.py
+++ chem3.py
@@ -7,12 +7,11 @@ d = {}
 
 a = input("Enter in a chemical compund: ")
 a1 = list(a)   #turns input into list
-
-for x in reversed(a1):    #reverses the list
+for x in reversed(a1):    #reverses the list(it's easier to seperate the elements when reversed)
     if x in lowercase:     #checks for lowercase
         l = x      #gives variable
     elif x in numbers:    #checks for numbers
-        n = x + n     #adds x to numbers variable
+        n = x + n     #adds x to numbers variable, the previous number would get overrided if not added
     elif x in uppercase:    #checks for uppercase
         if n == "":       #checks if there's no number before a lowercase/uppercase (remember it's going backwards)
             n = '1'      #makes the element(s) equal 1
